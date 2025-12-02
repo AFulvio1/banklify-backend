c8dd2175f255b0a7e7e092fec7a02df6ff5eba54
Antonio Fulvio <antoniofulvio@outlook.it>
2025-12-02 16:44:25 +0100

    add cors allowed origins for docker compose demo

--- src/main/java/com/afulvio/banklifybackend/security/SecurityConfig.java
+++ src/main/java/com/afulvio/banklifybackend/security/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173", "http://127.0.0.1:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
